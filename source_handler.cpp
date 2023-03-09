21b3a4d29728b217db0291cfb3f4062365c284e2
無冬 <123821243+aisyshk@users.noreply.github.com>
2023-03-09 11:16:42 -0700

    WIP

--- source_handler.cpp
+++ source_handler.cpp
@@ -1,31 +1,28 @@
-#include "source_handler.h"
+﻿#include "source_handler.h"
 
-const wchar_t* WAKA_URL = L"https://wakatime.com/api/v1/users/039e269a-c906-40c0-afd8-e33c509c9637/stats/all_time";
-const wchar_t* SAVE_FILE = L"C:\\waka_data.json";
+const wchar_t* WAKA_URL = L"https://wakatime.com/api/v1/users/@039e269a-c906-40c0-afd8-e33c509c9637/stats/all_time";
+const wchar_t* DATA_FILE = L"waka_data.json";
+const wchar_t* SAVE_FILE = L"C:\\processing_data.csv";
 
 WAKA_DATA WD{};
 
 int source_handler::FETCH_SOURCE()
 {
-	if (S_OK == URLDownloadToFile(NULL, WAKA_URL, SAVE_FILE, 0, NULL))
-	{
-		std::cout << "Downloaded Wakatime data. >> " << &SAVE_FILE << std::endl;
-		PARSE_DATA();
-	}
-	else { std::cerr << "Failed to download data."; }
+	//URLDownloadToFile(NULL, WAKA_URL, DATA_FILE, 0, NULL);
+	std::ifstream F(DATA_FILE);
+	json JSON_DATA = json::parse(F);
+
+	PARSE_DATA(JSON_DATA);
 
 	return 0;
 }
 
-int source_handler::PARSE_DATA()
+int source_handler::PARSE_DATA(json JSON_DATA)
 {
-	std::ifstream F(SAVE_FILE);
-	json JSON_DATA = json::parse(F);
-
 	if ((JSON_DATA["data"]["status"]) == "ok")
 	{
 		WD.STATUS = JSON_DATA["data"]["status"];
-		WD.DAILY_AVERAGE = JSON_DATA["data"]["human_readable_daily_average"];
+		WD.TOTAL_TIME = JSON_DATA["data"]["human_readable_total"];
 		
 		for (int i = 0; i < JSON_DATA["data"]["categories"].size(); i++)
 		{
@@ -37,24 +34,54 @@ int source_handler::PARSE_DATA()
 			WD.CATEGORIES.push_back(WCD);
 		}
 
-		for (int i = 0; i < JSON_DATA["data"]["languages"].size(); i++)
+		if (JSON_DATA["data"]["languages"].size() >= 5)
 		{
-			WAKA_LANGUAGE_DATA WLD{};
-			WLD.LANGUAGE = JSON_DATA["data"]["languages"][i]["name"];
-			WLD.TIME = JSON_DATA["data"]["languages"][i]["text"];
-			WLD.PERCENTAGE = JSON_DATA["data"]["languages"][i]["percent"];
+			for (int i = 0; i < 5; i++)
+			{
+				WAKA_LANGUAGE_DATA WLD{};
+				WLD.LANGUAGE = JSON_DATA["data"]["languages"][i]["name"];
+				WLD.TIME = JSON_DATA["data"]["languages"][i]["text"];
+				WLD.PERCENTAGE = JSON_DATA["data"]["languages"][i]["percent"];
 
-			WD.LANGUAGES.push_back(WLD);
+				WD.LANGUAGES.push_back(WLD);
+			}
+		}
+		else
+		{
+			for (int i = 0; i < JSON_DATA["data"]["languages"].size(); i++)
+			{
+				WAKA_LANGUAGE_DATA WLD{};
+				WLD.LANGUAGE = JSON_DATA["data"]["languages"][i]["name"];
+				WLD.TIME = JSON_DATA["data"]["languages"][i]["text"];
+				WLD.PERCENTAGE = JSON_DATA["data"]["languages"][i]["percent"];
+
+				WD.LANGUAGES.push_back(WLD);
+			}
 		}
 
-		for (int i = 0; i < JSON_DATA["data"]["projects"].size(); i++)
+		if (JSON_DATA["data"]["projects"].size() >= 5)
 		{
-			WAKA_PROJECTS_DATA WPD{};
-			WPD.PROJECT = JSON_DATA["data"]["projects"][i]["name"];
-			WPD.TIME = JSON_DATA["data"]["projects"][i]["text"];
-			WPD.PERCENTAGE = JSON_DATA["data"]["projects"][i]["percent"];
+			for (int i = 0; i < 5; i++)
+			{
+				WAKA_PROJECTS_DATA WPD{};
+				WPD.PROJECT = JSON_DATA["data"]["projects"][i]["name"];
+				WPD.TIME = JSON_DATA["data"]["projects"][i]["text"];
+				WPD.PERCENTAGE = JSON_DATA["data"]["projects"][i]["percent"];
 
-			WD.PROJECTS.push_back(WPD);
+				WD.PROJECTS.push_back(WPD);
+			}
+		}
+		else
+		{
+			for (int i = 0; i < JSON_DATA["data"]["projects"].size(); i++)
+			{
+				WAKA_PROJECTS_DATA WPD{};
+				WPD.PROJECT = JSON_DATA["data"]["projects"][i]["name"];
+				WPD.TIME = JSON_DATA["data"]["projects"][i]["text"];
+				WPD.PERCENTAGE = JSON_DATA["data"]["projects"][i]["percent"];
+
+				WD.PROJECTS.push_back(WPD);
+			}
 		}
 
 		for (int i = 0; i < JSON_DATA["data"]["editors"].size(); i++)
@@ -90,49 +117,90 @@ int source_handler::PARSE_DATA()
 
 int source_handler::OUTPUT_DATA()
 {
-	std::cout << "Status: " << WD.STATUS << 
-		" | Daily Avg.: " << WD.DAILY_AVERAGE <<
-		"\nUp to date?: " << WD.IS_UP_TO_DATE << std::endl << std::endl << std::endl;
+	std::cout << "[ Total Time ]" << std::endl << WD.TOTAL_TIME << std::endl;
 
-	std::cout << "\nCategories: " << std::endl;
+	std::cout << "\n[ Categories ]" << std::endl;
 	for (int i = 0; i < WD.CATEGORIES.size(); i++)
 	{
-		 std::cout << WD.CATEGORIES[i].CATEGORY << " | " << WD.CATEGORIES[i].TIME << " | " << WD.CATEGORIES[i].PERCENTAGE << std::endl;
+		 std::cout << "[" << WD.CATEGORIES[i].CATEGORY << "] " << WD.CATEGORIES[i].TIME << std::endl;
+		 CALCULATE_PROGRESS_BAR(WD.CATEGORIES[i].PERCENTAGE);
 	}
 
-	std::cout << "\nLanguages: " << std::endl;
+	std::cout << "\n[ Languages ]" << std::endl;
 	if (WD.LANGUAGES.size() > 5)
 	{
 		for (int i = 0; i < (WD.LANGUAGES.size() / 2); i++)
 		{
-			std::cout << WD.LANGUAGES[i].LANGUAGE << " | " << WD.LANGUAGES[i].TIME << " | " << WD.LANGUAGES[i].PERCENTAGE << std::endl;
+			std::cout << "[" << WD.LANGUAGES[i].LANGUAGE << "] " << WD.LANGUAGES[i].TIME << std::endl;
+			CALCULATE_PROGRESS_BAR(WD.LANGUAGES[i].PERCENTAGE);
 		}
 	}
 	else
 	{
 		for (int i = 0; i < WD.LANGUAGES.size(); i++)
 		{
-			std::cout << WD.LANGUAGES[i].LANGUAGE << " | " << WD.LANGUAGES[i].TIME << " | " << WD.LANGUAGES[i].PERCENTAGE << std::endl;
+			std::cout << "[" << WD.LANGUAGES[i].LANGUAGE << "] " << WD.LANGUAGES[i].TIME << std::endl;
+			CALCULATE_PROGRESS_BAR(WD.LANGUAGES[i].PERCENTAGE);
 		}
 	}
 
-	std::cout << "\nProjects: " << std::endl;
-	for (int i = 0; i < WD.PROJECTS.size(); i++)
+	std::cout << "\n[ Projects ]" << std::endl;
+	if (WD.PROJECTS.size() >= 5)
 	{
-		std::cout << WD.PROJECTS[i].PROJECT << " | " << WD.PROJECTS[i].TIME << " | " << WD.PROJECTS[i].PERCENTAGE << std::endl;
+		for (int i = 0; i < 5; i++)
+		{
+			std::cout << "[" << WD.PROJECTS[i].PROJECT << "] " << WD.PROJECTS[i].TIME << std::endl;
+			CALCULATE_PROGRESS_BAR(WD.PROJECTS[i].PERCENTAGE);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < WD.PROJECTS.size(); i++)
+		{
+			std::cout << "[" << WD.PROJECTS[i].PROJECT << "] " << WD.PROJECTS[i].TIME << std::endl;
+			CALCULATE_PROGRESS_BAR(WD.PROJECTS[i].PERCENTAGE);
+		}
 	}
 
-	std::cout << "\nEditors: " << std::endl;
+	std::cout << "\n[ Editors ] " << std::endl;
 	for (int i = 0; i < WD.EDITORS.size(); i++)
 	{
-		std::cout << WD.EDITORS[i].EDITOR << " | " << WD.EDITORS[i].TIME << " | " << WD.EDITORS[i].PERCENTAGE << std::endl;
+		std::cout << "[" << WD.EDITORS[i].EDITOR << "] " << WD.EDITORS[i].TIME << std::endl;
+		CALCULATE_PROGRESS_BAR(WD.EDITORS[i].PERCENTAGE);
 	}
 
-	std::cout << "\nOperating Systems: " << std::endl;
+	std::cout << "\n[ Operating Systems ] " << std::endl;
 	for (int i = 0; i < WD.OS.size(); i++)
 	{
-		std::cout << WD.OS[i].OS << " | " << WD.OS[i].TIME << " | " << WD.OS[i].PERCENTAGE << std::endl;
+		std::cout << "[" << WD.OS[i].OS << "] " << WD.OS[i].TIME << std::endl;
+		CALCULATE_PROGRESS_BAR(WD.OS[i].PERCENTAGE);
 	}
+
+	return 0;
+}
+
+int source_handler::CALCULATE_PROGRESS_BAR(DOUBLE PERCENT)
+{
+	int SMALLER_PERCENT = PERCENT / 2;
+	int MAX_PERCENT = 100 / 2;
+
+	std::cout << "[";
+	for (int i = 0; i < MAX_PERCENT; ++i)
+	{
+		if (i < SMALLER_PERCENT)
+		{
+			std::cout << "|";
+		}
+		/*else if (i == SMALLER_PERCENT)
+		{
+			std::cout << "-";
+		}*/
+		else
+		{
+			std::cout << " ";
+		}
+	}
+	std::cout << "] " << PERCENT << "%" << std::endl;
 
 	return 0;
 }
--- source_handler.h
+++ source_handler.h
@@ -20,8 +20,9 @@ class source_handler
 {
 public:
 	static int FETCH_SOURCE();
-	static int PARSE_DATA();
+	static int PARSE_DATA(json JSON_DATA);
 	static int OUTPUT_DATA();
+	static int CALCULATE_PROGRESS_BAR(DOUBLE PERCENT);
 };
 
 class WAKA_DATA
@@ -29,7 +30,7 @@ class WAKA_DATA
 public:
 	std::string STATUS;
 	std::vector<WAKA_CATEGORY_DATA> CATEGORIES;
-	std::string DAILY_AVERAGE;
+	std::string TOTAL_TIME;
 	std::vector <WAKA_LANGUAGE_DATA> LANGUAGES;
 	std::vector<WAKA_PROJECTS_DATA> PROJECTS;
 	std::vector<WAKA_EDITORS_DATA> EDITORS;
